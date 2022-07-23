47d73c13a77073975a56c13f091bd41ec72152f5
Yanahovsepyan <hovsepyanyana09@gmail.com>
2022-07-23 15:35:57 +0400

    check imgs that need to be deleted and downloaded

--- sync.py
+++ sync.py
@@ -1,7 +1,7 @@
+from unicodedata import name
 import requests
 from delete import removeImage
-
-deleteImageHost = "http://192.168.0.101:8888/"
+from downloadimg import getImage
 
 def getJson(url):
     try:
@@ -10,8 +10,19 @@ def getJson(url):
     except:
         return 0
 
-delImgs = dict(getJson(deleteImageHost))
-for img in delImgs:
-    # print('images/' + delImgs[img])
-    removeImage('images/' + delImgs[img])
+def getdeletedimgs():
+    
+    deleteImageHost = "http://192.168.0.101:8888/"
+
+    delImgs = dict(getJson(deleteImageHost))
+    for img in delImgs:
+        removeImage('images/' + delImgs[img])
+
+def getdownloadedimgs():
+
+    imgdownloadHost = "http://192.168.0.121:8888/core.php?getDeletedImages"
+     
+    downloadimgs = dict(getJson(imgdownloadHost))
+    for img in downloadimgs:
+        getImage('images/' + downloadimgs[img], downloadimgs[img])
     
